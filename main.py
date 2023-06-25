d7d5177dd884f8b2adfd1888401da06c02e46440
Macailh <salvadorenriquegb@gmail.com>
2023-06-24 21:11:42 -0600

    feat: add custom filenames

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from gtts import gTTS
 
 
-archivo = open("example.txt", "r", encoding="utf-8")
+txt_file = input("Ingrese el nombre del archivo de texto: ")
+audio_name = input("Ingrese el nombre que tendra el archivo de audio final: ")
+
+archivo = open(txt_file + ".txt", "r", encoding="utf-8")
 texto = archivo.read()
 archivo.close()
 
 tts = gTTS(text=texto, lang="es")
-tts.save("audio.mp3")
+tts.save(audio_name + ".mp3")
