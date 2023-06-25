2fd5515ba485ff316629e2d4bc59002e3bd31b8a
Macailh <salvadorenriquegb@gmail.com>
2023-06-24 21:15:08 -0600

    feat: add error handling for text file

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from gtts import gTTS
 
 
 txt_file = input("Ingrese el nombre del archivo de texto: ")
-audio_name = input("Ingrese el nombre que tendra el archivo de audio final: ")
 
-archivo = open(txt_file + ".txt", "r", encoding="utf-8")
-texto = archivo.read()
-archivo.close()
+try:
+    archivo = open(txt_file + ".txt", "r", encoding="utf-8")
+    texto = archivo.read()
+    archivo.close()
+except FileNotFoundError:
+    print("ERROR: No se pudo abrir el archivo de texto")
+    print("Verifique que el archivo se encuentre en la misma carpeta que el programa")
+    print("saliendo...")
+    exit()
+
+audio_name = input("Ingrese el nombre que tendra el archivo de audio final: ")
 
 tts = gTTS(text=texto, lang="es")
 tts.save(audio_name + ".mp3")
